878e429f6783a6fedc13bbac14e321c1e1e4a7d3
Leonid Logvinov <logvinov.leon@gmail.com>
2017-06-28 15:55:32 -0700

    Add registration_flow container

---
+++ ts/containers/registration_flow.tsx
@@ -0,0 +1,24 @@
+import * as _ from 'lodash';
+import {connect} from 'react-redux';
+import {Store as ReduxStore, Dispatch} from 'redux';
+import {Dispatcher} from 'ts/redux/dispatcher';
+import {State} from 'ts/redux/reducer';
+import {RegistrationFlow as RegistrationFlowComponent} from 'ts/pages/token_distribution/registration_flow';
+
+interface ConnectedDispatch {
+    dispatcher: Dispatcher;
+}
+interface RegistrationFlowComponentPassedProps {};
+
+const mapDispatchToProps = (dispatch: Dispatch<State>): ConnectedDispatch => ({
+    dispatcher: new Dispatcher(dispatch),
+});
+
+interface ConnectedState {
+    flashMessage?: string;
+};
+
+const mapStateToProps = (state: State): ConnectedState => ({flashMessage: state.flashMessage});
+
+export const RegistrationFlow: React.ComponentClass<RegistrationFlowComponentPassedProps> =
+  connect(mapStateToProps, mapDispatchToProps)(RegistrationFlowComponent);
