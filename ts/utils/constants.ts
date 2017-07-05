dfe8d2ed6fc81c13509eccdc9676fa51be859030
Leonid Logvinov <logvinov.leon@gmail.com>
2017-07-05 10:21:01 -0700

    Remove legacy ETHER_SCAN_ENDPOINT

--- ts/utils/constants.ts
+++ ts/utils/constants.ts
@@ -9,7 +9,6 @@ export const constants = {
     BITLY_ACCESS_TOKEN: 'ffc4c1a31e5143848fb7c523b39f91b9b213d208',
     BITLY_ENDPOINT: 'https://api-ssl.bitly.com',
     DEFAULT_TOKEN_ICON_URL: '/images/token_icons/default.png',
-    ETHER_SCAN_ENDPOINT: 'https://kovan.etherscan.io',
     ETHER_FAUCET_ENDPOINT: 'https://faucet.0xproject.com/rain',
     FEE_RECIPIENT_ADDRESS: '0x0000000000000000000000000000000000000000',
     HOSTED_TESTNET_URL: 'https://kovan.0xproject.com',
