bd45540c29e46ab1093150cf9b00aad90a9d9497
Fabio Berger <me@fabioberger.com>
2017-07-16 04:39:46 +0000

    missing renames from tokenDistribution to tokenLaunch

--- ts/index.tsx
+++ ts/index.tsx
@@ -6,7 +6,7 @@ import * as BigNumber from 'bignumber.js';
 import {configs} from 'ts/utils/configs';
 import {Home} from 'ts/pages/home/home';
 import {FAQ} from 'ts/pages/faq/faq';
-import {TokenDistribution} from 'ts/pages/token_distribution/token_distribution';
+import {TokenLaunch} from 'ts/pages/token_launch/token_launch';
 import {NotFound} from 'ts/pages/not_found';
 import {LazyComponent, createLazyComponent} from 'ts/lazy_component';
 import {State, reducer} from 'ts/redux/reducer';
@@ -85,7 +85,7 @@ render(
                         <Switch>
                             <Route exact={true} path="/" component={Home as any} />
                             <Route path="/otc" component={LazyOTC} />
-                            <Route path="/token" component={TokenDistribution as any} />
+                            <Route path="/token" component={TokenLaunch as any} />
                             <Route path="/faq" component={FAQ as any} />
                             <Route path="/docs/0xjs/:version?" component={LazyZeroExJSDocumentation} />
                             <Route component={NotFound as any} />
