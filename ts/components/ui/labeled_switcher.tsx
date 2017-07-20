ff5be60cc55b8582301ce1f997b61a9de74f5c77
Fabio Berger <me@fabioberger.com>
2017-07-20 15:34:32 +0200

    Update LabeledSwitcher to use boolean instead of enum since there are only two states. Also, handle the case where a switch fails, and gracefully revert the switcher to previous selection.

--- ts/components/ui/labeled_switcher.tsx
+++ ts/components/ui/labeled_switcher.tsx
@@ -4,42 +4,37 @@ import {colors} from 'material-ui/styles';
 
 const CUSTOM_BLUE = '#63A6F1';
 
-export enum Labels {
-    LEFT,
-    RIGHT,
-}
-
 interface LabeledSwitcherProps {
     labelLeft: string;
     labelRight: string;
-    initialSelectedLabel: Labels;
-    onLeftLabelClick: () => void;
-    onRightLabelClick: () => void;
+    isLeftInitiallySelected: boolean;
+    onLeftLabelClickAsync: () => void;
+    onRightLabelClickAsync: () => void;
 }
 
 interface LabeledSwitcherState {
-    selectedLabel: Labels;
+    isLeftSelected: boolean;
 }
 
 export class LabeledSwitcher extends React.Component<LabeledSwitcherProps, LabeledSwitcherState> {
     constructor(props: LabeledSwitcherProps) {
         super(props);
         this.state = {
-            selectedLabel: props.initialSelectedLabel,
+            isLeftSelected: props.isLeftInitiallySelected,
         };
     }
     public render() {
-        const isLeftLabelSelected = this.state.selectedLabel === Labels.LEFT;
+        const isLeft = true;
         return (
             <div
                 className="rounded clearfix"
             >
-                {this.renderLabel(this.props.labelLeft, Labels.LEFT, isLeftLabelSelected)}
-                {this.renderLabel(this.props.labelRight, Labels.RIGHT, !isLeftLabelSelected)}
+                {this.renderLabel(this.props.labelLeft, isLeft, this.state.isLeftSelected)}
+                {this.renderLabel(this.props.labelRight, !isLeft, !this.state.isLeftSelected)}
             </div>
         );
     }
-    private renderLabel(title: string, label: Labels, isSelected: boolean) {
+    private renderLabel(title: string, isLeft: boolean, isSelected: boolean) {
         const borderStyle = `2px solid ${isSelected ? '#4F8BCF' : '#DADADA'}`;
         const style = {
             cursor: 'pointer',
@@ -48,29 +43,34 @@ export class LabeledSwitcher extends React.Component<LabeledSwitcherProps, Label
             boxShadow: isSelected ? `inset 0px 0px 4px #4083CE` : 'inset 0px 0px 4px #F7F6F6',
             borderTop: borderStyle,
             borderBottom: borderStyle,
-            [label === Labels.LEFT ? 'borderLeft' : 'borderRight']: borderStyle,
+            [isLeft ? 'borderLeft' : 'borderRight']: borderStyle,
             paddingTop: 12,
             paddingBottom: 12,
         };
-        const isLeft = label === Labels.LEFT;
         return (
             <div
                 className={`col col-6 center p1 ${isLeft ? 'rounded-left' : 'rounded-right'}`}
                 style={style}
-                onClick={this.onLabelClick.bind(this, label)}
+                onClick={this.onLabelClickAsync.bind(this, isLeft)}
             >
                 {title}
             </div>
         );
     }
-    private onLabelClick(label: Labels) {
+    private async onLabelClickAsync(isLeft: boolean) {
         this.setState({
-            selectedLabel: label,
+            isLeftSelected: isLeft,
         });
-        if (label === Labels.LEFT) {
-            this.props.onLeftLabelClick();
+        let didSucceed;
+        if (isLeft) {
+            didSucceed = await this.props.onLeftLabelClickAsync();
         } else {
-            this.props.onRightLabelClick();
+            didSucceed = await this.props.onRightLabelClickAsync();
+        }
+        if (!didSucceed) {
+            this.setState({
+                isLeftSelected: !isLeft,
+            });
         }
     }
 }
