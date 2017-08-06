32d8fa405355d7468868362a7d959c56f6ed36d7
Fabio Berger <me@fabioberger.com>
2017-08-06 09:04:18 +0200

    Add live queueIt eventId and use it on production

--- ts/utils/configs.ts
+++ ts/utils/configs.ts
@@ -2,12 +2,13 @@ import * as _ from 'lodash';
 import {Environments} from 'ts/types';
 
 const BASE_URL = window.location.origin;
-const QUEUE_IT_CUSTOMER_ID = 'zeroex';
-const QUEUE_IT_EVENT_ID = 'test01';
-
 const isDevelopment = _.includes(BASE_URL, 'https://0xproject.dev:3572') ||
                       _.includes(BASE_URL, 'https://localhost:3572') ||
                       _.includes(BASE_URL, 'https://127.0.0.1');
+const QUEUE_IT_CUSTOMER_ID = 'zeroex';
+const QUEUE_IT_TEST_EVENT_ID = 'test01';
+const QUEUE_IT_LIVE_EVENT_ID = '0xtokensale';
+const queueItEventId = isDevelopment ? QUEUE_IT_TEST_EVENT_ID : QUEUE_IT_LIVE_EVENT_ID;
 
 export const configs = {
     BASE_URL,
@@ -16,7 +17,7 @@ export const configs = {
     IS_REGISTRATION_OPEN: true,
     RECAPTCHA_SITE_KEY: '6LcXHicUAAAAAOmRl4ZpDf2MxLEiHolYp1vpdOII',
     CIVIC_APP_ID: 'H1dfQuJEb',
-    QUEUE_IT_URL: `https://zeroex.queue-it.net/?c=${QUEUE_IT_CUSTOMER_ID}&e=${QUEUE_IT_EVENT_ID}`,
+    QUEUE_IT_URL: `https://zeroex.queue-it.net/?c=${QUEUE_IT_CUSTOMER_ID}&e=${queueItEventId}`,
     symbolsOfMintableTokens: ['MKR', 'MLN', 'GNT', 'DGD', 'REP'],
     mostPopularTradingPairSymbols: ['WETH', 'ZRX'],
     lastLocalStorageFillClearanceDate: '2017-07-07',
