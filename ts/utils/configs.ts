ae434d0fbcff75857ca6e5f0d52263ab51d2869f
Fabio Berger <me@fabioberger.com>
2017-08-13 08:27:52 -0500

    Set isRegistrationOpen to false and switch out QueueIt Event Id

--- ts/utils/configs.ts
+++ ts/utils/configs.ts
@@ -7,14 +7,14 @@ const isDevelopment = _.includes(BASE_URL, 'https://0xproject.dev:3572') ||
                       _.includes(BASE_URL, 'https://127.0.0.1');
 const QUEUE_IT_CUSTOMER_ID = 'zeroex';
 const QUEUE_IT_TEST_EVENT_ID = 'test01';
-const QUEUE_IT_LIVE_EVENT_ID = '0xtokensale';
+const QUEUE_IT_LIVE_EVENT_ID = '0xtokensale2';
 const queueItEventId = isDevelopment ? QUEUE_IT_TEST_EVENT_ID : QUEUE_IT_LIVE_EVENT_ID;
 
 export const configs = {
     BASE_URL,
     ENVIRONMENT: isDevelopment ? Environments.DEVELOPMENT : Environments.PRODUCTION,
     BACKEND_BASE_URL: isDevelopment ? 'https://localhost:3001' : 'https://api.0xproject.com',
-    IS_REGISTRATION_OPEN: true,
+    IS_REGISTRATION_OPEN: false,
     IS_CONTRIBUTE_OPEN: false,
     RECAPTCHA_SITE_KEY: '6LcXHicUAAAAAOmRl4ZpDf2MxLEiHolYp1vpdOII',
     CIVIC_APP_ID: 'H1dfQuJEb',
