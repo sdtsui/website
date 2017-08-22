b8c5301b4c9d95154b4c4e2d6144cee49fca1331
Fabio Berger <me@fabioberger.com>
2017-08-22 10:18:56 +0200

    remove unused methods

--- ts/web3_wrapper.ts
+++ ts/web3_wrapper.ts
@@ -37,17 +37,6 @@ export class Web3Wrapper {
         }
         return (addresses as string[])[0];
     }
-    public async sendTransactionAsync(txParams: any): Promise<string> {
-        const transactionHex = await promisify(this.web3.eth.sendTransaction)(txParams);
-        return transactionHex;
-    }
-    public async getTransactionReceiptIfExistsAsync(txHash: string): Promise<Web3.TransactionReceipt|undefined> {
-        const receiptIfExists = await promisify(this.web3.eth.getTransactionReceipt)(txHash);
-        if (_.isNull(receiptIfExists)) {
-            return undefined;
-        }
-        return receiptIfExists;
-    }
     public async getNodeVersionAsync() {
         const nodeVersion = await promisify(this.web3.version.getNode)();
         return nodeVersion;
