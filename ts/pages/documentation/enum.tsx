4aeb1e865ccf96b29dd5b22d2e8695e85c331f09
Fabio Berger <me@fabioberger.com>
2017-08-24 20:30:15 +0200

    put /n at front instead of back

--- ts/pages/documentation/enum.tsx
+++ ts/pages/documentation/enum.tsx
@@ -13,12 +13,12 @@ export function Enum(props: EnumProps) {
     const values = _.map(props.values, (value, i) => {
         const isLast = i === props.values.length - 1;
         const defaultValueIfAny = !_.isUndefined(value.defaultValue) ? ` = ${value.defaultValue}` : '';
-        return `\t${value.name}${defaultValueIfAny},${!isLast ? '\n' : ''}`;
+        return `\n\t${value.name}${defaultValueIfAny},`;
     });
     return (
         <span>
             {`{`}
-                {'\n'}{values}
+                {values}
                 <br />
             {`}`}
         </span>
